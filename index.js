ad150846cb0b1c593e9c8a90bd5695faf7ed4d96
gravitymir <gravitymir@gmail.com>
2020-08-23 01:53:15 +0300

    renew build

--- index.js
+++ index.js
@@ -1,121 +1,76 @@
 require("./process.env.js");
-const zadarma = require("./v1/index.js");
+const z_api = require("./v1/index.js");
 
 
 (async function(){
-    //console.log(zadarma);
+    let response;
+    /*
+    response = await z_api.request({
+        api_method: '/v1/request/callback/',
+        params: {
+            from: '380947102794',
+            to: '380989897908',
+            sip: '100',
+            predicted: 'predicted'
+        }
+    });
+    console.log(response)
+    */
+    response = await z_api.request({
+        http_method: 'post', 
+        api_method: '/v1/sms/send/',
+        params: {
+            caller_id: '380947102794',
+            number: '380989897908',//'67200000000',
+            message: 'nodejs fff ыфва ыыц'
+        }
+    });
+    console.log(response)
+    /*
     
-    // console.log(await zadarma.infoBalance());
-    // console.log(await zadarma.infoPrice({to: 67200000000}));
-    // console.log(await zadarma.infoTimezone());
-    // console.log(await zadarma.tariff());
+    response = await z_api.request({api_method: '/v1/info/balance/'});
+    console.log(response)
 
-    // console.log(await zadarma.requestCallback({
-    //     from: '380947102794',
-    //     to: '380989897908',
-    //     sip: 100,
-    //     predicted: true
-    
-    // }));
+    response = await z_api.request({api_method: '/v1/info/timezone/'});
+    console.log(response)
 
-    //console.log(await zadarma.sip());
-    //console.log(await zadarma.sipStatus('990126'));
+    response = await z_api.request({api_method: '/v1/tariff/'});
+    console.log(response)
 
+    response = await z_api.request({api_method: '/v1/sip/'});
+    console.log(response)
 
-    //error put
-    // console.log(await zadarma.sipChangeCallerId({
-    //     id: '886493',
-    //     number: your number
-    // }));
+    let sip_number = '886493';
 
-    //console.log(await zadarma.sipRedirection('990126'));
-    //console.log(await zadarma.directNumbers());
+    response = await z_api.request({api_method: '/v1/sip/${sip_number}/status/'});
+    console.log(response)
 
-    //error put
-    // console.log(await zadarma.sipOnOffRedirection({
-    //     id: '886493',
-    //     status: 'off'
-    // }));
+    response = await z_api.request({api_method: '/v1/direct_numbers/'});
+    console.log(response)
 
-    //error put
-    // console.log(await zadarma.sipChangeRedirection({
-    //     id: '886493',
-    //     type: 'phone',
-    //     number: 'off'
-    // }));
+    response = await z_api.request({api_method: '/v1/zcrm/customers'});
+    console.log(response)
 
-    //console.log(await zadarma.pbxInternal());
-    //console.log(await zadarma.pbxInternalStatus(100));
+    response = await z_api.request({
+        http_method: 'dalete',
+        api_method: '/v1/zcrm/customers/c_id',
+        params: {
+            c_id: '1111111'
+        }
+    });
+    console.log(response)
 
-    //error put
-    // console.log(await zadarma.pbxInternalOnOffRecording({
-    //     id: 100,
-    //     status: 'on',
-    //     email: ''
-    // }));
+    response = await z_api.request({
+        //http_mathod: 'get', [default]
+        api_method: '/v1/info/price/',
+        params: {
+            number: '67200000000',
+            caller_id: '73919100000'
+        }
+    });
 
-    //error put
-    // console.log(await zadarma.smsSend({
-    //     number: '380989897908',
-    //     message: 'test text'
-    // }));
-    //console.log(await zadarma.statisticsPbx(100));
+    console.log(response);
+    */
 }
 )();
 ;
-
-
-let o = {
-    values: [9,8,7,6,5,4,3,2,1],
-    [Symbol.iterator](){
-        let i = 0;
-        return {
-            next: () => {
-                const value = this.values[i];
-                i++
-                return {
-                    done: i > this.values.length,
-                    value
-                }
-            }
-        }
-    }
-}
-
-
-for (let item of o){
-    console.log(item);
-}
-
-
-const str = 'asdasd';
-console.log(str.startsWith('as'));
-console.log(str.endsWith('d'));
-
-console.log(str.includes('d'));
-
-console.log([1,2,3].includes(2));
-console.log('asd'.repeat(2));
-console.log('asd'.padStart(6, 'asd'));
-
-
-console.log(' asd '.trim(2));
-console.log(' asd '.trimStart(2));
-
-
-function abc(arr){
-    return arr.reduce((acc, i) => {
-        return acc += i
-    }, 0) / arr.length
-}
-
-console.log(abc([10,20,30,40]));
-
-let s = {
-    s: 10
-}
-
-let a = s;
-
-console.log(Object.is(s, a));
-
